e24d22ea21852121a262f9ce601107ebf525fcee
HYUN WOOK KANG <kangtaepoong@naver.com>
2024-03-26 08:32:15 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 seed_four_path=/home/hyunwook_kang/SEED-IV/eeg_feature_smooth
-deap_path=/home/hyunwook_kang/data_preprocessed_matlab
 
-data_paths=("ConTL" "DGCNN")
+ModelArray=("ConTL" "DGCNN")
 
 
 save_file_name=seed4_result.csv
 
-python main.py --w-mode w --model_type ConTL --data-path $seed_four_path --data-choice 4 --n-classes 4 --save_file_name $save_file_name --lstm
+#python main.py --w-mode w --model_type ConTL --data-path $seed_four_path --data-choice 4 --n-classes 4 --save_file_name $save_file_name --lstm
 
 for model in ${ModelArray[@]}; do
     python main.py --w-mode a --model_type $model --data-path $seed_four_path --data-choice 4 --n-classes 4 --save_file_name $save_file_name 
