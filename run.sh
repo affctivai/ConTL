2d5942144e5686ee17ff4f641147a88e6c13701c
HYUN WOOK KANG <kangtaepoong@naver.com>
2024-06-19 08:17:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ ModelArray=("ConTL" "DGCNN")
 
 save_file_name=seed4_result.csv
 
-python main.py --w-mode w --model_type ConTL --data-path $seed_four_path --data-choice 4 --n-classes 4 --save_file_name $save_file_name --lstm 
+python main.py --w-mode w --model_type ConTL --data-path $seed_four_path --data-choice 4 --save_file_name $save_file_name --lstm 
 
 
 for model in ${ModelArray[@]}; do
-    python main.py --w-mode a --model_type $model --data-path $seed_four_path --data-choice 4 --n-classes 4 --save_file_name $save_file_name 
+    python main.py --w-mode a --model_type $model --data-path $seed_four_path --data-choice 4 --save_file_name $save_file_name 
 done
 
 
